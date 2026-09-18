466a5d6200b06432adef8b5a0117cbd85121f06f
dtdarpan21-gif <dtdarpan21@gmail.com>
2026-09-18 12:32:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
-//complete this code
-class Rectangle {}
+// complete this code
+class Rectangle {
+    private _width: number;
+    private _height: number;
 
-class Square extends Animal {}
+    constructor(width: number, height: number) {
+        this._width = width;
+        this._height = height;
+    }
+
+    get width(): number {
+        return this._width;
+    }
+
+    get height(): number {
+        return this._height;
+    }
+
+    getArea(): number {
+        return this._width * this._height;
+    }
+}
+
+class Square extends Rectangle {
+    constructor(side: number) {
+        super(side, side);
+    }
+
+    getPerimeter(): number {
+        return this.width * 4;
+    }
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
